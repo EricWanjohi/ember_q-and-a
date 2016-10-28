22c5ba6acd496b97225460c11d8501f036a1b92f
Sara Jay <jensese@MacBook-Pro-2.local>
2016-10-28 13:29:29 -0700

    add new-answer form, and display it on the individual question route. Save new answer to the question object, but also to the answer property of the question object.

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    cancel() {
+      this.set('addNewAnswer', false);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        answer: this.get('answer'),
+        question: this.get('question'),
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
