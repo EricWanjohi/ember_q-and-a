e87015c2c4ec80c01e3999b5690cd0e9d2c42b73
Sara Jensen <jensen.sara.e@gmail.com>
2016-11-04 10:54:37 -0700

    add bookmark question service to app, add button to question to bookmark questions, add route to show questions that have been bookmarked.

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  bookmarkQuestion: Ember.inject.service(),
+
   actions: {
     delete(question) {
       if(confirm('Are you sure you want to delete this question?')) {
@@ -9,6 +11,9 @@ export default Ember.Component.extend({
     },
     destroyAnswer(answer) {
       this.sendAction('destroyAnswer', answer);
+    },
+    addToBookmarks(question) {
+      this.get('bookmarkQuestion').add(question);
     }
   }
 });
